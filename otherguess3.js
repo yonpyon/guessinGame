2cd501ba9254ffb186c6adfe2a9129ccd7d768c2
Mika <mkeilani43@gmail.com>
2015-11-05 20:09:38 -0800

    inializing

---
+++ otherguess3.js
@@ -0,0 +1,12 @@
+
+			var p= prompt("did you lose any finngers in the past").toLowerCase();
+
+			if (p === "no"){
+            	alert("congradulations!!!you have ten filangies! the world works perfectly for you:DDDDD");
+			}
+
+			else{
+				var k = prompt("how many???? integers only (1,2,3 ect.)");
+				alert("you have " + (10 - k)  + " fingers..... i hope life is treating you okay. This world is made for ten fingers.... im sorry as a ten finger person.");
+			}
+
